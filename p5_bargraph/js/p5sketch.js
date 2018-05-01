40370b6021f3ded47975c880724577050043efc7
Sam Fine <samafine1@gmail.com>
2018-05-01 10:46:46 -0700

    Update p5sketch.js

--- p5_bargraph/js/p5sketch.js
+++ p5_bargraph/js/p5sketch.js
@@ -51,7 +51,7 @@ function setup() {
         strokeWeight(1);
         stroke(time);
 
-        rect(x, (y/10000)+750, x + spacing, (y/10000)+750 - sensor);
+        rect(x, (-y/1000), x + spacing, (-y/1000) + sensor);
 
         textAlign(CENTER);
         fill(255);
